fa15cef52e5a3d61399074df5239e712dc0b721b
Eduardo Maia <eduvimaia@gmail.com>
2013-05-31 19:50:32 -0300

    fixing undefined browser version

--- jquery.meio.mask.js
+++ jquery.meio.mask.js
@@ -34,11 +34,27 @@
 (function($) {
     
     if(!$.browser){
+        // https://github.com/jquery/jquery-migrate/blob/master/src/core.js#L50
+        //
+        uaMatch = function(ua) {
+          ua = ua.toLowerCase();
+
+          var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
+              /(webkit)[ \/]([\w.]+)/.exec(ua) ||
+              /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
+              /(msie) ([\w.]+)/.exec(ua) ||
+              ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) ||
+              [];
+
+          return match[2] || "0";
+        };
+
         $.browser = {};
         $.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase());
         $.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
         $.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
         $.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
+        $.browser.version = uaMatch(navigator.userAgent);
     }
     
     var isIphone = (window.orientation != null);

09eb717673b6aa161f3ccbd8cf53211ee8f3cc3c
Fabio M. Costa <fabiomcosta@gmail.com>
2013-06-03 18:39:30 -0700

    removing .zip creation and the file itself

--- build.py
+++ build.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 import os
-from zipfile import ZipFile, BadZipfile
 
 
 class Builder:
 
     javascript_files = []
-    extra_zip_files = []
 
     def __init__(self, minify_postfix='min', extension='js'):
         self.minify_posfix = minify_postfix
@@ -46,41 +44,16 @@ class Builder:
         os.system('java -jar assets/yui-compressor/yui.jar --warn --charset utf8 ' + self.file_name + self.extension + ' > ' + self.file_name + '.' + self.minify_posfix + self.extension)
         print '** Succesfully created minified file. **'
 
-    def create_zip_file(self):
-        zip_file = ZipFile(self.file_name + '.zip', 'w')
-
-        file_name = self.file_name + self.extension
-        zip_file.write(file_name, self.file_name + '/' + file_name)
-
-        compressed_file_name = self.file_name + '.' + self.minify_posfix + self.extension
-        zip_file.write(compressed_file_name, self.file_name + '/' + compressed_file_name)
-
-        for name, absolute_name in self.extra_zip_files:
-            zip_file.write(absolute_name, self.file_name + '/' + name)
-
-        zip_file.close()
-
-        #tests the zipfile
-        zip_file = ZipFile(self.file_name + '.zip', 'r')
-        if zip_file.testzip() is not None:
-            raise BadZipfile()
-        else:
-            print '** Succesfully created zipped file. **'
-        zip_file.close()
-
-    def build(self, file_name, files, extra_zip_files={}, root='Source/'):
+    def build(self, file_name, files, root='Source/'):
         self.file_name = file_name
         self.add_files('javascript_files', files, root=root, extension=self.extension)
-        self.add_files('extra_zip_files', extra_zip_files)
 
         print 'Starting to build ' + file_name + ' files...'
         self.create_built_file()
         self.create_minified_file()
-        self.create_zip_file()
         print ''
 
         self.javascript_files = []
-        self.extra_zip_files = []
 
 if __name__ == '__main__':
     builder = Builder()

464d2b0dc48dab534815e2a2afc5324ce4340315
John Voloski <johnvoloski@cwi.com.br>
2013-11-06 11:17:23 -0200

    remove jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,29 +15,16 @@ module.exports = function(grunt) {
         src:  'dist/<%= pkg.name %>.js',
         dest: 'dist/<%= pkg.name %>.min.js'
       }
-    },
-    jasmine: {
-      pivotal: {
-        src: ['src/<%= pkg.name %>.js', 'spec/javascripts/helpers', 'spec/javascripts/fixtures'],
-        options: {
-          specs: 'spec/javascripts/*_spec.js',
-          helpers: 'spec/javascripts/helpers/*.js'
-        }
-      }
     }
   });
 
   // Loaded Plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Build Task(s)
   grunt.registerTask('build', ['concat', 'uglify']);
 
-  // Test Task(s)
-  grunt.registerTask('test', ['jasmine']);
-
   // Travis CI
-  grunt.registerTask('travis', ['jasmine']);
+  grunt.registerTask('travis', ['build']);
 };
